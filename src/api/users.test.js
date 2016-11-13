e26ce1fe9780c70567b20106e44cc5b84b9acf5c
Ethan Apodaca <papodaca@gmail.com>
2016-11-13 13:09:03 -0800

    Update user api test.

--- src/api/users.test.js
+++ src/api/users.test.js
@@ -2,8 +2,20 @@ import expect from 'expect';
 
 import userApi from './users'
 
-describe('Api to manage users', ()=> {
-  it('should pass exist', () => {
-    expect(true).toEqual(true);
+describe('Api to manage users: ', () => {
+  it('should exist', () => {
+    expect(userApi).toExist();
+    expect(userApi.getAllUsers).toExist();
+    expect(userApi.addUser).toExist();
+    expect(userApi.get).toExist();
+    expect(userApi.update).toExist();
+    expect(userApi.delete).toExist();
+  });
+  describe('getAllUsers', () => {
+    it('should return data', () => {
+      let data = userApi.getAllUsers();
+      expect(data.length).toBe(20);
+      expect(data[0].name).toExist();
+    });
   });
 });
